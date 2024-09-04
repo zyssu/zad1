11e8b6bf8be06f8eddb7c401c737e9d69bc79723
Damian Stefański <damian.stefanski2@teb.edu.pl>
2024-09-04 17:25:11 +0200

    Dodaj pliki projektów.

---
+++ Praktyki/zad1/Car.cs
@@ -0,0 +1,19 @@
+﻿namespace Praktyki.zad1;
+
+public class Car
+{
+    public string Model;
+    public string Color;
+    public string Brand;
+    public int YearOfProduction;
+    public double Mileage;
+
+    public Car(string model, string color, string brand, int yearOfProduction, double mileage)
+    {
+        this.Model = model;
+        this.Color = color;
+        this.Brand = brand;
+        this.YearOfProduction = yearOfProduction;
+        this.Mileage = mileage;
+    }
+}
---
+++ Praktyki/zad1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+
+namespace Praktyki.zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Aventador SVJ", "Red", "Lamborghini", 2020, 7500);
+            Car car2 = new Car("Mustang", "Blue", "Ferrari", 2019, 12000.7);
+
+            Console.WriteLine($"Car 1: {car1.Model}, {car1.Color}, {car1.Brand}, {car1.YearOfProduction}, {car1.Mileage} km");
+            Console.WriteLine($"Car 2: {car2.Model}, {car2.Color}, {car2.Brand}, {car2.YearOfProduction}, {car2.Mileage} km");
+        }
+    }
+}
